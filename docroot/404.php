e4189dc841e2dd32848691231df86f76cd3e597c
Glen Campbell <glen@broadpool.com>
2013-03-24 22:24:08 -0500

    changed footer links

--- docroot/404.php
+++ docroot/404.php
@@ -16,14 +16,14 @@ date_default_timezone_set('America/Chicago');
   The management of this site wishes to express its regret that the page
   or script you have requested could not be found. It is with our deepest
   sorrow that we offer our profound apologies for this sad state of affairs,
-  even though it was probably your own fucking fault. 
+  even though it was probably your own fucking fault.
   </marquee>
   <div id="box">
     <p id="top">NOT FOUND</p>
     <p id="day">404</p>
     <p id="bottom"><a href="/">Take me home</a></p>
   </div>
-  <p id="copyright">&copy;<?php echo date('Y');?> <a href="http://suburbanredneck.net" title="Perhaps the most amazing site on the Internet. Perhaps not.">Glen Campbell</a>
+  <p id="copyright">&copy;<?php echo date('Y');?> <a href="http://glen-campbell.com/" title="Perhaps the most amazing site on the Internet. Perhaps not.">Glen Campbell</a>
   &bull; <a href="/timezone" title="Click here to change your timezone">Timezone</a></p>
 </body>
 </html>
