13369d9c3f7cd27cda844e44f569b903ec538ce3
Glen Campbell <glen@broadpool.com>
2013-06-26 10:31:12 -0500

    moved to template structure

--- docroot/templates/default.php
+++ docroot/templates/default.php
@@ -1,3 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>TODAY-NOW</title>
+<meta http-equiv="content-type" content="text/html; charset=utf-8"/>
+<meta name="generator" content="Glen Campbell: http://glenc.co/"/>
+<meta name="description" content="Calendar and clock: a single-function site"/>
+<meta name="keywords" content="calendar,clock,date,time"/>
+<!-- Timezone is <?php echo $TZ;?> -->
+<style type="text/css">
 body {
 <?php if (strtoupper($BG) == 'KITTENS') { ?>
   background: #fff url('kittens.jpg') no-repeat fixed center;
@@ -73,3 +83,20 @@ label {
   font-size: smaller;
   text-align: left;
 }
+</style>
+</head>
+<body onload="ajax(page,'clock')">
+<div id="content">
+  <div id="box">
+    <p id="top"><?php echo date('l');?> <span id="clock" title="Time not right? Click Settings, below, to choose your timezone."><?php echo date($TF);?></span></p>
+    <p id="day"><?php echo date('j');?></p>
+    <p id="bottom"><?php echo date('F Y');?></p>
+  </div>
+</div>
+<p id="copyright">&copy;<?php echo date('Y');?>
+ <a href="http://glen-campbell.com/"
+ title="Perhaps the most amazing site on the Internet. Perhaps not.">Glen Campbell</a>
+&bull; <a href="/help" title="Click here if you need assistance">Help</a>
+&bull; <a href="/settings" title="Click here to change your time zone, format, and other settings">Settings</a></p>
+</body>
+</html>
