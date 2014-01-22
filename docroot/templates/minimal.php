90d1c54f73fe23fd830c322881316fa1920380dc
Glen Campbell <glen@glenc.io>
2014-01-22 15:31:07 -0600

    changed the appearance

--- docroot/templates/minimal.php
+++ docroot/templates/minimal.php
@@ -2,38 +2,50 @@
 <html>
 <head>
 <title>TODAY-NOW</title>
-<meta http-equiv="content-type" content="text/html; charset=utf-8"/>
-<meta name="generator" content="Glen Campbell: http://glenc.co/"/>
-<meta name="description" content="Calendar and clock: a single-function site"/>
-<meta name="keywords" content="calendar,clock,date,time"/>
+<meta http-equiv="content-type" content="text/html; charset=utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta name="generator" content="Glen Campbell: http://glenc.co/">
+<meta name="description" content="Calendar and clock: a single-function site">
+<meta name="keywords" content="calendar,clock,date,time">
 <!-- <?php echo basename(__FILE__); ?> -->
 <!-- Timezone is <?php echo $TZ;?> -->
-<link href='http://fonts.googleapis.com/css?family=Life+Savers' rel='stylesheet' type='text/css'>
 <style type="text/css">
 body { 
-font-family: 'Life Savers', Helvetica, cursive; 
-margin: 5em 0 5em 40px;
+font-family: Helvetica, Arial, cursive; 
+margin: 24% auto;
+text-align: center;
 }
 #box { 
-font-size: 2em; 
-line-height: 1em;
-border-bottom: 1px silver dotted;
+font-size: 12pt;
 }
 #copyright { 
 font-family: helvetica, arial, serif;
-font-size: 80%; 
-position: absolute;
-bottom: 0;
+font-size: 10px; 
 margin-bottom: 10px;
+color: silver;
+text-align: center;
+margin-top: 20%;
+}
+#date {
+font-size: 14px;
+display: block;
 color: gray;
+text-transform: uppercase;
+letter-spacing: 3px;
+}
+#clock {
+display: block;
+font-weight: bold;
+color: black;
+font-size: 50px;
 }
 a, a:hover, a:link, a:visited {
-color: gray;
+color: silver;
 text-decoration: underline;
 }
 a:hover {
 text-decoration: none;
-font-style: italic;
+color: gray;
 }
 </style>
 </head>
@@ -45,7 +57,7 @@ font-style: italic;
   </div>
 </div>
 <p id="copyright">&copy;<?php echo date('Y');?>
- <a href="http://glen-campbell.com/"
+ <a href="http://glenc.io/"
  title="Perhaps the most amazing site on the Internet. Perhaps not.">Glen Campbell</a>
 &bull; <a href="/help" title="Click here if you need assistance">Help</a>
 &bull; <a href="/settings" title="Click here to change your time zone, format, and other settings">Settings</a></p>
