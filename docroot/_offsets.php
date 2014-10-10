cba12361e00866cc97d9d6b7a79471bc035ccc57
Glen Campbell <glen@glenc.io>
2014-10-10 12:42:48 -0500

    client-side, baby

--- docroot/_offsets.php
+++
@@ -1,9 +0,0 @@
-<?php
-$tzmap = array();
-foreach(timezone_identifiers_list() as $timezone) {
-	$time = new dateTime(time(), new DateTimeZone($timezone));
-	$offset = substr($time->format('O'), 0, 9);
-	if (!isset($tzmap[$offset])) $tzmap[$offset] = $timezone;
-}
-foreach($tzmap as $offset => $timezone)
-	printf("  '%s' => '%s',\n", $offset, $timezone);
