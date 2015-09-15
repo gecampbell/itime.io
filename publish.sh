7082654763fdf85cbd0c54fdfb37abf57f9c07db
Glen Campbell <glen@broadpool.com>
2015-09-15 16:03:56 -0700

    cache-control

--- publish.sh
+++ publish.sh
@@ -2,7 +2,7 @@
 
 if [ -d site ]; then
     cd site
-    aws s3 sync . s3://itime.io
+    aws s3 sync . s3://itime.io --cache-control "max-age=600"
 else
     echo "You're not in the right directory"
     exit
